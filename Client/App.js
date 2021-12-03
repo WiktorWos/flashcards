e9d890cab88fa634bb11d18815cbae2772845f37
WiktorWos <wiktor.wos99@gmail.com>
2021-12-03 20:35:39 +0100

    quick actions view

--- Client/App.js
+++ Client/App.js
@@ -2,7 +2,8 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import Auth from "./components/auth/auth";
+import QuickActions from "./components/quick-actions/quickActions";
 
 export default function App() {
-  return <Auth />;
+  return <QuickActions />;
 }
